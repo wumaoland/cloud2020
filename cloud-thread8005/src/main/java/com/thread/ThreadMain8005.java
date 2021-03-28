d7a6b0dd8fd3dfcb2ca8b77232b1364ac35fa357
liwei <zllw14623@sina.cn>
2021-03-28 20:38:32 +0800

    提交线程学习、websocket学习demo

---
+++ cloud-thread8005/src/main/java/com/thread/ThreadMain8005.java
@@ -0,0 +1,18 @@
+package com.thread;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @ClassName ThreadMain8005
+ * @Description TODO
+ * @Author lw
+ * @Date 2021/3/28 0028 20:22
+ * @Vsersion 1.0
+ **/
+@SpringBootApplication
+public class ThreadMain8005 {
+    public static void main(String[] args) {
+        SpringApplication.run(ThreadMain8005.class, args);
+    }
+}
