224677d8be1035ec3779727fc7b42d596c0ff5cb
liwei <wei.li@jian24.com>
2021-08-06 15:24:01 +0800

    修改RocketMq的消费者注释

--- cloud-rocketmq8003/src/main/java/com/rocketmq/basic/SpringConsumer.java
+++ cloud-rocketmq8003/src/main/java/com/rocketmq/basic/SpringConsumer.java
@@ -1,21 +1,21 @@
 package com.rocketmq.basic;
 
 import org.apache.rocketmq.spring.annotation.ConsumeMode;
+import org.apache.rocketmq.spring.annotation.MessageModel;
 import org.apache.rocketmq.spring.annotation.RocketMQMessageListener;
 import org.apache.rocketmq.spring.core.RocketMQListener;
 import org.springframework.stereotype.Component;
 
 /**
  * @ClassName SpringConsumer
- * @Description TODO
+ * @Description  MessageModel.BROADCASTING是广播模式；MessageModel.CLUSTERING是集群消费模式
  * @Author lw
  * @Date 2021/3/13 0013 22:30
  * @Vsersion 1.0
  **/
 @Component
-@RocketMQMessageListener(consumerGroup = "MyConsumerGroup",topic = "TestTopic",consumeMode = ConsumeMode.CONCURRENTLY)
+@RocketMQMessageListener(consumerGroup = "MyConsumerGroup",topic = "TestTopic",consumeMode = ConsumeMode.CONCURRENTLY,messageModel = MessageModel.BROADCASTING)
 public class SpringConsumer implements RocketMQListener<String> {
-
 
     @Override
     public void onMessage(String message) {
