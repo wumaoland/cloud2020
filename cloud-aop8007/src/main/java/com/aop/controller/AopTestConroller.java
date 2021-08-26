89ec7a9a295f87e4886278fa4d627969fa2a0f8f
lw <zllw14623@sina.cn>
2021-08-26 21:33:56 +0800

    使用AOP和RateLimiter限流器

--- cloud-aop8007/src/main/java/com/aop/controller/AopTestConroller.java
+++ cloud-aop8007/src/main/java/com/aop/controller/AopTestConroller.java
@@ -2,6 +2,7 @@ package com.aop.controller;
 
 import com.aop.annotation_method.AfterThrowingAopLog;
 import com.aop.annotation_method.AroundAopLog;
+import com.aop.annotation_method.RedissonApiLimit;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,14 +13,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class AopTestConroller {
 
     //@BeforeAopLog
-    @AroundAopLog
+    //@AroundAopLog
     //@AfterAopLog
     //@AfterReturningAopLog
     //@AfterThrowingAopLog
+    @RedissonApiLimit(tps = 1,rateTime = 10)
     @GetMapping("/testAop/{code}")
     public String testAop(@PathVariable("code") String code) {
         log.info("业务方法执行");
-        int a = 3 / 0;
         return "业务方法执行成功";
     }
 }
