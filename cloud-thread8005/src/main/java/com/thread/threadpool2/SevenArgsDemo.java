f47e3bf178c0fdb0cf3f97f6146cd656bfbb69d9
lw <zllw14623@sina.cn>
2021-09-12 13:06:00 +0800

    线程数最大线程数定义

--- cloud-thread8005/src/main/java/com/thread/threadpool2/SevenArgsDemo.java
+++ cloud-thread8005/src/main/java/com/thread/threadpool2/SevenArgsDemo.java
@@ -8,10 +8,18 @@ import java.util.concurrent.*;
  * 2.四大拒绝策略：
  * new ThreadPoolExecutor.AbortPolicy()  //线程拒绝策略：当前这个策略是当超出最大可承载线程数(最大运行线程数+阻塞队列的长度=8)，
  * 就抛出异常,三大方法默认都是这个策略
+ *
  * new ThreadPoolExecutor.CallerRunsPolicy()  //线程拒绝策略：哪来的回哪去，例如下面第九个线程就交个main线程去处理了
+ *
  * new ThreadPoolExecutor.DiscardPolicy() //队列满了，丢掉任务，不抛出异常
+ *
  * new ThreadPoolExecutor.DiscardOldestPolicy();//队列满了，尝试和最早的竞争，也不会抛出异常！
  * 被拒绝任务的处理程序，丢弃最早的未处理请求，然后重试execute ，除非执行程序关闭，在这种情况下任务将被丢弃
+ *
+ * 3.最大线程数确定
+ * 线程池最大线程数如何定义
+ * CPU密集型 几个逻辑处理器就是几，可以保持CPU的效率最高，比如我的电脑就是6核12个逻辑处理器，最大线程数可以填12
+ * IO密集型  数量 >判断你程序中十分耗IO的线程数，一般是两倍
  */
 public class SevenArgsDemo {
     public static void main(String[] args) {
@@ -28,10 +36,10 @@ public class SevenArgsDemo {
                 new ThreadPoolExecutor.AbortPolicy()//线程拒绝策略：当前这个策略是当超出最大可承载线程数(最大运行线程数+阻塞队列的长度=8)，就抛出异常,三大方法默认都是这个策略
         );
         //new ThreadPoolExecutor.CallerRunsPolicy() //线程拒绝策略：哪来的回哪去，例如下面第九个线程就交个main线程去处理了
-        //new ThreadPoolExecutor.DiscardPolicy() //线程拒绝策略：哪来的回哪去，例如下面第九个线程就交个main线程去处理了
-        //new ThreadPoolExecutor.DiscardOldestPolicy();//队列满了，尝试和最早的竞争，也不会抛出异常！
+        //new ThreadPoolExecutor.DiscardPolicy() //线程拒绝策略：队列满了，丢掉任务，不抛出异常
+        //new ThreadPoolExecutor.DiscardOldestPolicy();//队列满了，丢弃最早的未处理请求，然后重试execute,也不会抛出异常！
         try {
-            for (int i = 1; i <= 9; i++) {
+            for (int i = 1; i <= 8; i++) {
                 threadPool.execute(() -> {
                     System.out.println(Thread.currentThread().getName() + ":ok");
                 });
