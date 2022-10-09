5b92f90a30cb5b92b4928fa373139f5a09f5b197
lw <lw@inesa-t.com>
2022-10-09 14:02:55 +0800

    测试

--- java-base/src/base/ArrayTest.java
+++ java-base/src/base/ArrayTest.java
@@ -4,4 +4,11 @@ package base;
  * List测试
  */
 public class ArrayTest {
+    public static void main(String[] args) {
+        String abc="你好|我是";
+        String[] split = abc.split("\\|");
+        for (String s : split) {
+            System.out.println(s);
+        }
+    }
 }
